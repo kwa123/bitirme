ebb0f11dd931529368a8cd6f525de7d5b4279753
ONUR_KIVANC BOZKURT <bozkurt.okivanc@student.atilim.edu.tr>
2013-03-21 23:44:20 +0200

    kwa dan

--- Contact/src/kwa/module/contact/ViewContact.java
+++ Contact/src/kwa/module/contact/ViewContact.java
@@ -1,7 +1,12 @@
 package kwa.module.contact;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -17,7 +22,48 @@ public class ViewContact extends Activity{
 	        newc=(Button)findViewById(R.id.newContact);
 	        home=(Button)findViewById(R.id.home);
 	        
+	        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+					this);
+	 
+				// set title
+				alertDialogBuilder.setTitle("Deleting");
+	 
+				// set dialog message
+				alertDialogBuilder
+					.setMessage("Delete contact ?")
+					.setCancelable(false)
+					.setPositiveButton("YES",new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog,int id) {
+							
+							
+						}
+					  })
+					.setNegativeButton("NO",new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog,int id) {
+							// if this button is clicked, just close
+							// the dialog box and do nothing
+							dialog.cancel();
+						}
+					});
+	 
+					// create alert dialog
+					final AlertDialog alertDialog = alertDialogBuilder.create();
+	 
+					// show it
+					
+	        
 	        ListView contactView=(ListView)findViewById(R.id.listContact);
+	        contactView.setClickable(true);
+	        contactView.setOnLongClickListener(new View.OnLongClickListener() {
+				
+				public boolean onLongClick(View v) {
+					// TODO Auto-generated method stub
+					// v.getId();
+					Log.e("", v.getId()+"");
+					return false;
+				}
+			});
+	        
 	        
 	        
 	    }
